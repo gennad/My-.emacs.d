14b1a541d92bbcb7cd954f69f60bfc26453c3aea
Gennadiy Zlobin <g.zlobin@rambler-co.ru>
2012-09-16 22:05:02 +0400

    Some changes

--- emacs-for-python/extensions/open-next-line.el
+++ emacs-for-python/extensions/open-next-line.el
@@ -8,10 +8,10 @@
   (next-line 1)
   (when newline-and-indent
     (indent-according-to-mode)))
-(global-set-key (kbd "C-o") 'open-next-line)
+(global-set-key (kbd "C-j") 'open-next-line)
 ;; Behave like vi's O command
 (defun open-previous-line (arg)
-  "Open a new line before the current one. 
+  "Open a new line before the current one.
      See also `newline-and-indent'."
   (interactive "p")
   (beginning-of-line)
